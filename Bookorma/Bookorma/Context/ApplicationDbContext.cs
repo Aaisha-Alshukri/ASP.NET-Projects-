a84c2954f6999c0b454041da023e649e8ba3c5a9
Aaisha-Alshukri <aishalshukri7@gmail.com>
2025-05-31 22:51:53 +0400

    Added Bookorma project

---
+++ Bookorma/Bookorma/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Bookorma.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bookorma.Context
+{
+    public class ApplicationDbContext : DbContext
+    { 
+      public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Bookorma/Bookorma/Controllers/BookController.cs
@@ -0,0 +1,64 @@
+﻿using Bookorma.Context;
+using Bookorma.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookorma.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Find(id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _context.Books.Find(id);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
